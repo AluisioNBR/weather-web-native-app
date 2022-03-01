59d6508bcbe3f5a543886f839df17eaa8259c30b
AluisioNBR <lulu70638@gmail.com>
2022-02-28 22:21:06 -0300

    bug fixed 10

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ function CitySelection({ setTemperatureVisibility, setCityName, setCountry, setT
     return infJSON
   }
   
-  async function submitCity(event){
+  async function submitCity(event: SubmitEvent){
     event.preventDefault()
     const inf = await WeatherInf()
 
@@ -35,11 +35,11 @@ function CitySelection({ setTemperatureVisibility, setCityName, setCountry, setT
 
   return(
       <div>
-        <form onSubmit={(event) => submitCity(event)} id={styles.formCity}>
+        <form onSubmit={(event: SubmitEvent) => submitCity(event)} id={styles.formCity}>
           <label htmlFor='nameInput'>
             Informe sua cidade:
           </label><br />
-          <input name='nameInput' required defaultValue={cityValue} onChange={(event) => setCityValue(event.target.value)}/>
+          <input name='nameInput' required defaultValue={cityValue} onChange={(event: ChangeEvent) => setCityValue(event.target.value)}/>
 
           <button>
             Selecionar
