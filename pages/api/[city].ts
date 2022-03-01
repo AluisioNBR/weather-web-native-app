c53193d7ea3a51abcd774f133a87af25ef641eef
AluisioNBR <lulu70638@gmail.com>
2022-02-28 21:54:53 -0300

    bug fixed 4

--- pages/api/[city].ts
+++ pages/api/[city].ts
@@ -14,14 +14,14 @@ async function WeatherInf(
   const APIdata = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityReq},BR&appid=${process.env.API_KEY}&lang=pt_br`)
   const APIdataJSON = await APIdata.json()
 
-  interface Response{
+  interface Data{
     city: string,
     country: string,
     temperature: number,
     description: string
   }
 
-  const ResponseJSON: Response = {
+  const ResponseJSON: Data = {
     city: APIdataJSON.name,
     country: APIdataJSON.sys.country,
     temperature: (Number(APIdataJSON.main.temp) - 273),
