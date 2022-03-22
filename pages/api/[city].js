a348ba10c0b2353a9bb14870c6c95fce9ee49ae2
AluisioNBR <lulu70638@gmail.com>
2022-03-22 19:17:57 -0300

    refatory api

--- pages/api/[city].js
+++ pages/api/[city].js
@@ -1,47 +1,52 @@
-async function WeatherInf(req, res) {
-  const cityReq = req.query.city;
+import axios from 'axios'
 
-  const APIdata = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityReq},BR&appid=${process.env.API_KEY}&lang=pt_br`
-  );
-  const APIdataJSON = await APIdata.json();
-
-  if (APIdataJSON.cod === 200) {
-    const city = `${APIdataJSON.name}`;
-    const temperature = Math.floor(Number(APIdataJSON.main.temp) - 273);
-    const feels_like = Math.floor(Number(APIdataJSON.main.feels_like) - 273);
-    const temp_min = Math.floor(Number(APIdataJSON.main.temp_min) - 273);
-    const temp_max = Math.floor(Number(APIdataJSON.main.temp_max) - 273);
-    const humidity = APIdataJSON.main.humidity;
-    const icon = `http://openweathermap.org/img/w/${APIdataJSON.weather[0].icon}.png`;
-    const description = APIdataJSON.weather[0].description;
-
-    res.status(200).json({
-      cod: 200,
-      msg: "Cidade encontrada!",
-      city: city,
-      country: "BR",
-      temperature: temperature,
-      main: {
-        feels_like: feels_like,
-        temp_min: temp_min,
-        temp_max: temp_max,
-        humidity: humidity
-      },
-      icon: icon,
-      description: description
-    });
-  } else if (APIdataJSON.cod === 404) {
-    res.status(200).json({
-      cod: 404,
-      msg: "Cidade não encontrada! Tente outra cidade!"
-    });
-  } else {
-    res.status(APIdataJSON.cod).json({
-      cod: APIdataJSON.cod,
-      msg: "Ocorreu um problema em nosso servidor! Tente novamente mais tarde!"
-    });
+async function fecthAndReturnApiData(city){
+  try {
+    const data = await axios.get('https://api.openweathermap.org/data/2.5/weather?', {
+      params: {
+        q: `${city},BR`,
+        lang: 'pt_br',
+        units: 'metric',
+        appid: process.env.API_KEY
+      }
+    })
+    return await data.data;
+  } catch (error) {
+    return {
+      cod: 404
+    }
   }
 }
 
-export default WeatherInf;
+function goodRequestData(data){
+  return {
+    city: `${data.name}`,
+    temperature: Math.floor(data.main.temp) + 1,
+    feels_like: Math.floor(data.main.feels_like) + 1,
+    temp_min: Math.floor(data.main.temp_min) + 1,
+    temp_max: Math.floor(data.main.temp_max) + 1,
+    humidity: Math.floor(data.main.humidity) + 1,
+    icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+    description: data.weather[0].description
+  }
+}
+
+function notFoundDataOfRequest(){
+  return {
+    cod: 404,
+    msg: "Cidade não encontrada! Tente outra cidade!"
+  }
+}
+
+function verifyApiData(data){
+  if (data.cod === 200) return goodRequestData(data)
+  else return notFoundDataOfRequest()
+}
+
+async function returnWeatherInformations(req, res) {
+  const data = await fecthAndReturnApiData(req.query.city)
+  const responseForUser = verifyApiData(data)
+  res.status(data.cod).json(responseForUser)
+}
+
+export default returnWeatherInformations;
