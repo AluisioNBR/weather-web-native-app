ff6eb7e9dd13c03a4c236c6546f706ef5abbaa29
AluisioNBR <lulu70638@gmail.com>
2022-04-05 10:57:29 -0300

    newAPI add

--- pages/api/[city].js
+++ pages/api/[city].js
@@ -1,5 +1,34 @@
 import axios from 'axios'
 
+export default async function returnInformations(req, res) {
+  if(req.query.myApiSecret === process.env.MY_API_SECRET){
+    const data = await verifyCity(req.query.city)
+    const responseForUser = verifyApiData(data)
+    res.status(200).json(responseForUser)
+  }
+  else res.status(200).json({
+    cod: 403,
+    msg: "Você não tem autorização para fazer esta requisição!"
+  })
+}
+
+async function verifyCity(city){
+  const localizationOptions = {
+    notFound: (local) => { throw new Error(`${local.name} not found!`) },
+    found: (local) => fecthAndReturnWeatherData(local)
+  }
+  const localization = await fecthAndReturnGeocodingLocalizationEndpointData(city)
+  try {
+    const weatherData = localizationOptions[localization.found](localization.local)
+    return weatherData
+  }
+  catch (error) {
+    return {
+      weather: { cod: 404 }
+    }
+  }
+}
+
 async function fecthAndReturnGeocodingLocalizationEndpointData(city){
   try {
     const localization = await axios.get('http://api.openweathermap.org/geo/1.0/direct?', {
@@ -10,30 +39,32 @@ async function fecthAndReturnGeocodingLocalizationEndpointData(city){
       }
     })
     const local = await localization.data
+    if(local == []) throw new Error('Cidade não encontrada!')
     return {
-      cod: 200,
+      found: 'found',
       local: local[0]
     }
   }
   catch(err){
     return {
-      cod: 404
+      found: 'notFound',
     }
   }
 }
 
-async function fecthAndReturnCurrentWeatherEndpointData(local){
+async function fecthAndReturnWeatherData(local){
   try {
-    const data = await axios.get('https://api.openweathermap.org/data/2.5/weather?', {
+    const data = await axios.get('https://api.openweathermap.org/data/2.5/onecall?', {
       params: {
         lat: local.lat,
         lon: local.lon,
         lang: 'pt_br',
         units: 'metric',
+        exclude: 'minutely,alerts',
         appid: process.env.API_KEY
       }
     })
-    const weatherData = await data.data
+    const weatherData = splitWeatherDataType(await data.data)
     return {
       weather: weatherData,
       localization: {
@@ -49,34 +80,65 @@ async function fecthAndReturnCurrentWeatherEndpointData(local){
   }
 }
 
-async function verifyCity(city){
-  const localization = await fecthAndReturnGeocodingLocalizationEndpointData(city)
-  try {
-    if(localization.cod === 404) throw new Error('City not found!')
-    else return fecthAndReturnCurrentWeatherEndpointData(localization.local)
-  }
-  catch (error) {
-    return {
-      weather: { cod: 404 }
-    }
+function splitWeatherDataType(data){
+  return {
+    current: data.current,
+    hourly: data.hourly,
+    daily: data.daily
   }
 }
 
+function verifyApiData(data){
+  return (data.weather.cod === 404) || (data.weather.cod === 400) ? notFoundDataOfRequest(): foundDataOfRequest(data)
+}
+
+/*
+* data.weather.current
+* data.weather.hourly
+* data.weather.daily
+*/
+
+// TODO Add os valores reais da nova API
 function foundDataOfRequest(data){
+  const currentWeatherData = formatCurrentWeather(data.weather.current)
   return {
-    cod: data.weather.cod,
+    cod: 200,
     city: `${data.localization.city}`,
     state: `${data.localization.state}`,
-    temperature: Math.floor(data.weather.main.temp) + 1,
-    main: {
-      feels_like: Math.floor(data.weather.main.feels_like) + 1,
-      temp_min: Math.floor(data.weather.main.temp_min) + 1,
-      temp_max: Math.floor(data.weather.main.temp_max) + 1,
-      humidity: Math.floor(data.weather.main.humidity) + 1,
-    },
-    icon: `http://openweathermap.org/img/w/${data.weather.weather[0].icon}.png`,
-    description: data.weather.weather[0].description
+    current: currentWeatherData
   }
+}
+
+function formatCurrentWeather(data){
+  return {
+    temp: formatTemperature(data.temp),
+    feels_like: formatTemperature(data.feels_like),
+    uvi: data.uvi,
+    description: data.weather[0].description,
+    icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+    rain: ifRainy(data.weather[0].main, data.rain['1h']),
+    snow: ifSnowed(data.weather[0].main, data.snow['1h'])
+  }
+}
+
+function formatTemperature(temp){
+  const [intPartNumber, floatPartNumber] = `${temp}`.split('.')
+  if(Number(floatPartNumber) >= 50) intPartNumber += 1
+  return intPartNumber
+}
+
+function ifRainy(main, rain){
+  return main === "Rain" ? {
+    rainy: 'rain',
+    rain: rain
+  }: { rainy: 'no-rain' }
+}
+
+function ifSnowed(main, snow){
+  return main === "Snow" ? {
+    snowed: 'snow',
+    snow: snow
+  }: { snowed: 'no-snow' }
 }
 
 function notFoundDataOfRequest(){
@@ -85,23 +147,3 @@ function notFoundDataOfRequest(){
     msg: "Cidade não encontrada! Tente outra cidade!"
   }
 }
-
-function verifyApiData(data){
-  if (data.weather.cod === 200) return foundDataOfRequest(data)
-  else return notFoundDataOfRequest()
-}
-
-async function returnInformations(req, res) {
-  if(req.query.myApiSecret === process.env.MY_API_SECRET){
-    const data = await verifyCity(req.query.city)
-    const responseForUser = verifyApiData(data)
-    res.status(200).json(responseForUser)
-  }
-  else res.status(200).json({
-    cod: 403,
-    msg: "Você não tem autorização para fazer esta requisição!"
-  })
-  
-}
-
-export default returnInformations;
