4d0f8347f2533be4ddf04b85a5bf86cd59e73a2d
AluisioNBR <lulu70638@gmail.com>
2022-04-24 12:19:45 -0300

    date update 4

--- components/api/formatGenericalData.js
+++ components/api/formatGenericalData.js
@@ -43,8 +43,10 @@ function formatDays(dateReq) {
   const days = [], month = (Number(dateReq[1]) - 1), year = Number(dateReq[2])
   let day = Number(dateReq[0])
   for(let cont = 0; cont < 8; cont++){
-    const date = new Date(year, month, day)
-    days.push(date)
+    const date = new Date(year, month, day), dateSplited = `${date}`.split(' ')
+    const dayToReturn = Number(dateSplited[2]) < 10 ? `0${dateSplited[2]}`: dateSplited[2]
+    const monthToReturn = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}`: date.getMonth() + 1
+    days.push(`${dateSplited[2]}/${monthToReturn}/${dateSplited[3]}`)
     day += 1
   }
   return days
