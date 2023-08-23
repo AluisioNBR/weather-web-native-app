c72f83203c96817f4034378a6e6a36aaa7db4a76
AluisioNBR <lulu70638@gmail.com>
2023-08-23 18:16:22 -0300

    bug fixed 2

--- components/submitCity/weatherStateReducer.ts
+++ components/submitCity/weatherStateReducer.ts
@@ -3,82 +3,83 @@ import type {
   CurrentWeather,
   HourWeather,
   DayWeather,
-  WeatherReducerAction
-} from '../../types/submitCity/weatherStateReducer.types'
+  WeatherReducerAction,
+} from "../../types/submitCity/weatherStateReducer.types";
 
 const weatherInitialValue: WeatherState = {
-  city: '',
-  state: '',
+  city: "",
+  state: "",
   currentWeather: {
     temp: 0,
     feels_like: 0,
     uvi: 0,
     humidity: 0,
-    description: '',
-    icon: '',
-    rain: { rainy: 'no-rain'},
-    snow: { snowed: 'no-snow'}
+    description: "",
+    main: "",
+    icon: "",
+    rain: { rainy: "no-rain" },
+    snow: { snowed: "no-snow" },
   },
   hourlyWeather: [],
-  dailyWeather: []
-}
+  dailyWeather: [],
+};
 
-function weatherReducer(state: WeatherState, action: WeatherReducerAction){
-  let value: string | CurrentWeather | HourWeather[] | DayWeather[]
+function weatherReducer(state: WeatherState, action: WeatherReducerAction) {
+  let value: string | CurrentWeather | HourWeather[] | DayWeather[];
   switch (action.type) {
-    case 'city':
-      value = action.value as string
+    case "city":
+      value = action.value as string;
       return {
         city: value,
         state: state.state,
         currentWeather: state.currentWeather,
         hourlyWeather: state.hourlyWeather,
-        dailyWeather: state.dailyWeather
+        dailyWeather: state.dailyWeather,
       };
-    
-    case 'state':
-      value = action.value as string
+
+    case "state":
+      value = action.value as string;
       return {
         city: state.city,
         state: value,
         currentWeather: state.currentWeather,
         hourlyWeather: state.hourlyWeather,
-        dailyWeather: state.dailyWeather
+        dailyWeather: state.dailyWeather,
       };
-    
-    case 'currentWeather':
-      value = action.value as CurrentWeather
+
+    case "currentWeather":
+      value = action.value as CurrentWeather;
       return {
         city: state.city,
         state: state.state,
         currentWeather: value,
         hourlyWeather: state.hourlyWeather,
-        dailyWeather: state.dailyWeather
+        dailyWeather: state.dailyWeather,
       };
-      
-    case 'hourlyWeather':
-      value = action.value as HourWeather[]
+
+    case "hourlyWeather":
+      value = action.value as HourWeather[];
       return {
         city: state.city,
         state: state.state,
         currentWeather: state.currentWeather,
         hourlyWeather: value,
-        dailyWeather: state.dailyWeather
+        dailyWeather: state.dailyWeather,
       };
-      
-    case 'dailyWeather':
-      value = action.value as DayWeather[]
+
+    case "dailyWeather":
+      value = action.value as DayWeather[];
       return {
         city: state.city,
         state: state.state,
         currentWeather: state.currentWeather,
         hourlyWeather: state.hourlyWeather,
-        dailyWeather: value
+        dailyWeather: value,
       };
-  
+
     default:
       return state;
   }
 }
 
-export { weatherInitialValue, weatherReducer }
+export { weatherInitialValue, weatherReducer };
